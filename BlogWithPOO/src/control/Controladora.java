26df5289a54f890d96cc500a88088e437b30db4f
Joan Steven Cordero Coto <j.cordero.2@estudiantec.cr>
2026-04-30 08:53:13 -0600

    Refactor Controladora to add blog and publication methods

--- BlogWithPOO/src/control/Controladora.java
+++ BlogWithPOO/src/control/Controladora.java
@@ -1,31 +1,77 @@
 package control;
 
-import logica.Blog;
 import java.util.Map;
+import java.util.TreeMap;
+
+import logica.Blog;
 
 public class Controladora {
-    private Map<Integer, Blog> Blogs;
+
+    private Map<Integer, Blog> blogs;
 
     public Controladora() {
-        blogs = new TreeMap<Integer, Blog>;
+        blogs = new TreeMap<Integer, Blog>();
     }
 
-    public void crearBlog(String nombre, string descripcion) {
+    public void crearBlog(String nombre, String descripcion) {
         Blog b = new Blog(nombre, descripcion);
         blogs.put(b.getCodigo(), b);
     }
 
-    public Map<Integer, string> obtenerBlogs() {
+    public void borrarBlog(int codigoBlog) throws Exception {
+        if (!blogs.containsKey(codigoBlog))
+            throw new Exception("El codigo de blog no es valido.");
+
+        blogs.remove(codigoBlog);
+    }
+
+    public Map<Integer, String> obtenerBlogs() {
         Map<Integer, String> resultado = new TreeMap<Integer, String>();
+
         for (Blog b : blogs.values()) {
             resultado.put(b.getCodigo(), b.getNombre());
         }
+
         return resultado;
     }
 
-    public void crearPublicacion(int codigoBlog, String titulo, String texto, String nombre) throws Exception {
+    public void crearPublicacion(int codigoBlog, String titulo, String texto, String nombreCreador) throws Exception {
         if (!blogs.containsKey(codigoBlog))
             throw new Exception("El codigo de blog no es valido.");
+
         Blog b = blogs.get(codigoBlog);
-        b.crearPublicacion(titulo, texto, nombre);
+        b.agregarPublicacion(titulo, texto, nombreCreador);
     }
+
+    public Map<Integer, String> obtenerPublicaciones(int codigoBlog) throws Exception {
+        if (!blogs.containsKey(codigoBlog))
+            throw new Exception("El codigo de blog no es valido.");
+
+        Blog b = blogs.get(codigoBlog);
+        return b.listarTitulos();
+    }
+
+    public String obtenerPublicacion(int codigoBlog, int codigoPublicacion) throws Exception {
+        if (!blogs.containsKey(codigoBlog))
+            throw new Exception("El codigo de blog no es valido.");
+
+        Blog b = blogs.get(codigoBlog);
+        return b.getPublicacionString(codigoPublicacion);
+    }
+
+    public void agregarComentario(int codigoBlog, int codigoPublicacion, String email, String ip, String texto) throws Exception {
+        if (!blogs.containsKey(codigoBlog))
+            throw new Exception("El codigo de blog no es valido.");
+
+        Blog b = blogs.get(codigoBlog);
+        b.agregarComentario(codigoPublicacion, email, ip, texto);
+    }
+
+    public void borrarComentario(int codigoBlog, int codigoPublicacion, int posicion) throws Exception {
+        if (!blogs.containsKey(codigoBlog))
+            throw new Exception("El codigo de blog no es valido.");
+
+        Blog b = blogs.get(codigoBlog);
+        b.eliminarComentario(codigoPublicacion, posicion);
+    }
+}
